9f91d66a37c8cf7d4ecfa7c584488d835e65a600
hafiz47 <hafizarslanamjad47@gmail.com>
2024-11-08 18:27:41 -0500

    linting4

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 # src/config.py
 import os
 
+
 class Config:
-    # Set the database URI to use SQLite
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'quiz_app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+    os.getcwd(), 'quiz_app.db'
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = False
